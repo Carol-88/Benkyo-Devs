b3ccb461c56fe91a4f84e58f5e258b2150ed2df5
Lourdes Dols <113938248+LourdesDols@users.noreply.github.com>
2023-04-24 23:13:19 +0200

    migración a nextjs

--- Card.js
+++ Card.js
@@ -1,70 +1,96 @@
 import styles from "./card.module.css";
 import { useState, useEffect } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
-// const questions = [];
-
-function props() {
-    const [questionActual, setQuestionActual] = useState(0);
-    const [answerActual, setAnswerActual] = useState (0);
-    const [correctans, setCorrectans] = useState(false);
-    const [finishedLvl, setFinishedLvl] = useState(false);
-
-    function handleAnswwerSubmit(){
-      //añadir estilos de preguntas (añadir pregunta y cual es la correcta)
-      e.target.classList.add(correct ? "corrects" : "incorrects");
-      //cambiar a la siguiente pregunta
-
-      setTimeout(() => {
-        if (questionActual === questions.length - 1) {
-          setFinishedLvl(true);
-        } else {
-          setQuestionActual(questionActual + 1);
-        }
-      }, 1500);
-      
-     
-    }
+export default function Card() {
+  const router = useRouter();
 
 
-    if(finishedLvl) return (
-      <main className="card">
-        <div className="game.finished">
-          <span></span>
-          <button onClick={() = (window.location.href = "/")}>Volver a jugar</button>
-        </div>
-        </main>
-    )
-    
+  const [questionActual, setQuestionActual] = useState(0);
+  const [answerActual, setAnswerActual] = useState (0);
+  const [correctans, setCorrectans] = useState(false);
+  const [finishedLvl, setFinishedLvl] = useState(false);
+
+  function handleAnswwerSubmit(){
+    //añadir estilos de preguntas (añadir pregunta y cual es la correcta)
+    e.target.classList.add(correct ? "corrects" : "incorrects");
+    //cambiar a la siguiente pregunta
+
+    setTimeout(() => {
+      if (questionActual === questions.length - 1) {
+        setFinishedLvl(true);
+      } else {
+        setQuestionActual(questionActual + 1);
+      }
+    }, 2000);
+  }
+
+
+  // if(finishedLvl) return (
+  //   <main className="card">
+  //     <div className="game.finished">
+  //       <span></span>
+  //       <button onClick={() => (window.location.href = "/")}>Volver a jugar</button>
+  //     </div>
+  //   </main>
+  // )
+  if (finishedLvl) {
     return (
-    <main className="card">
-      <article>
-        <section className="tittleques">
-          <span> {questions.[questionActual].question} </span>
-        </section>
+      <>
+        <Head>
+          <title>Juego terminado</title>
+        </Head>
+        <main className={styles.card}>
+          <div className={styles.gameFinished}>
+            <span></span>
+            <button onClick={() => router.push("/")}>Volver a jugar</button>
+          </div>
+        </main>
+      </>
+    );
+  }
 
-        <section className="answerques">
-       {questions[questionActual].answers.map((respuesta) = (
-            <button key={answers.correct} onClick={() = handleAnswwerSubmit()} > {answers.correct} </button>
-        ))}
-        </section>
 
-        <section className="answerques2">
-       {questions[questionActual].answers.map((respuesta) = (
-            <button key={answers.correct} onClick={() = handleAnswwerSubmit()} > {answers.correct} </button>
-        ))}
-        </section>
+  return (
+    <>
+      <Head>
+        <title>Preguntas y respuestas</title>
+      </Head>
+      <main className={styles.card}>
+        <article>
+          <section className={styles.titleQuestion}>
+            <span>{questions.[questionActual].question}</span>
+          </section>
 
-        <section className="difficultlvl">
-          <h2>Dificultad</h2>
-          <li>Baja</li>
-          <li>Media</li>
-          <li>Alta</li>
-        </section>
+          <section className={styles.answerQuestion}>
+            {questions[questionActual].answers.map((respuesta) => (
+              <button key={answers.correct} onClick={() => handleAnswerSubmit()}>
+                {answers.correct}
+              </button>
+            ))}
+          </section>
 
-        <section className="favanswer">
-          Añadir pregunta a favoritos
-        </section>
-      </article>
-    </main>
+          <section className={styles.answerQuestion2}>
+            {questions[questionActual].answers.map((respuesta) => (
+              <button key={answers.correct} onClick={() => handleAnswerSubmit()}>
+                {answers.correct}
+              </button>
+            ))}
+          </section>
+
+          <section className={styles.difficultyLevel}>
+            <h2>Dificultad</h2>
+            <li>Baja</li>
+            <li>Media</li>
+            <li>Alta</li>
+          </section>
+
+          <section className={styles.favoriteAnswer}>
+            Añadir pregunta a favoritos
+          </section>
+        </article>
+      </main>
+    </>
   );
 }
