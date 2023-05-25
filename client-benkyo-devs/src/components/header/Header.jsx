37f24a99da5a5ab0e3b9907da47da1ad7b2b63d9
Irwing Naranjo <khalisser@gmail.com>
2023-05-25 12:06:04 -0400

    Fix difficult component se arreglan las imagenes
    - Se corrigen las imágenes rotas
    - Se agrega funcionalidad de selección
    - Se renombra el componente a index
    - Se usa alias en la importación

--- client-benkyo-devs/src/components/header/Header.jsx
+++ client-benkyo-devs/src/components/header/Header.jsx
@@ -1,4 +1,14 @@
-import Difficult from "./difficult/difficult";
+/*
+  Recomendación
+  Al crear componentes es una buena práctica si el componente es el principal dentro de una carpeta
+  dejarlo con el nombre index.jsx, esto para que cuando se importe el componente no sea necesario
+  escribir el nombre del archivo, solo la carpeta.
+  en este ejemplo esto ./difficult en los mismo que ./difficult/index
+
+  el uso de alias es una buena práctica para evitar escribir rutas largas y relativas
+  esto @/components/header/difficult es lo mismo que ./difficult
+*/
+import Difficult from "@/components/header/difficult";
 import UserInfo from "./user/user-info";
 
 const Header = () => {
