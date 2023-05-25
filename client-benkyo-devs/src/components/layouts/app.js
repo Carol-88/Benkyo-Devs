1b85adf8daa302adf0cad6fcf5d4292a0230920b
Irwing Naranjo <khalisser@gmail.com>
2023-05-25 12:25:53 -0400

    Refactor Layout Component se puede cambiar los valores de SEO por cada página

--- client-benkyo-devs/src/components/layouts/app.js
+++ client-benkyo-devs/src/components/layouts/app.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Header from '@/components/header/Header';
 
+const DEFAULT_SEO = {
+  title: 'Benkyo Devs',
+  description: 'App de estudio de lenguajes de programacion',
+}
+
 /*
   El uso de Layout es una práctica muy común en React
   para evitar repetir importar componentes comunmente usados
@@ -10,15 +15,15 @@ import Header from '@/components/header/Header';
   el componente Header y Footer, y de renderizarlos en cada
   página donde se use Layout minimizando la cantidad de código.
 */
-export default function Layout({ title = '', children }) {
+export default function Layout({ seo = DEFAULT_SEO, children }) {
+
+  const { title, description } = seo;
+
   return (
     <>
       <Head>
-        <title>Benkyo Devs</title>
-        <meta
-          name="description"
-          content="App de estudio de lenguajes de programacion"
-        />
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- client-benkyo-devs/src/pages/index.js
+++ client-benkyo-devs/src/pages/index.js
@@ -1,9 +1,14 @@
 import Card from "carol-88/components/card/Card";
 import Layout from "carol-88/components/layouts/app";
 
+const SEO = {
+  title: "Benkyo Devs - Game",
+  description: "Juego para estudiar lenguajes de programacion",
+};
+
 export default function Home() {
   return (
-    <Layout>
+    <Layout seo={SEO}>
       <main>
         <Card />
       </main>
