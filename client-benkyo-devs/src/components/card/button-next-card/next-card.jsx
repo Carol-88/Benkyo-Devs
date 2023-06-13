a08e38bed8ff719953a5594ccd8f0d44c1f9b923
Carol-88 <gralleta@gmail.com>
2023-06-13 10:43:49 +0200

    corrijo imagen en com. next-card

--- client-benkyo-devs/src/components/card/button-next-card/next-card.jsx
+++ client-benkyo-devs/src/components/card/button-next-card/next-card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 function NextCard({ handleClick }) {
   return (
@@ -6,7 +7,7 @@ function NextCard({ handleClick }) {
       onClick={handleClick}
       className="flex justify-center items-center p-5 rounded-xl w-40"
     >
-      <img src={"img/flecha-correcta.png"} width={100} />
+      <Image src={"img/flecha-correcta.png"} width={100} height={100} alt="Flecha para cambiar de tarjeta" />
     </button>
   );
 }
