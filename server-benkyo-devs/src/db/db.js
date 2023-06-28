b366e0a3ed630925a8a534d2e454dd736bec2f57
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-06-28 17:44:18 +0200

    tryCatch para gestionar error conexion a BBDD

--- server-benkyo-devs/src/db/db.js
+++ server-benkyo-devs/src/db/db.js
@@ -4,21 +4,24 @@ const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 
 let pool;
 
-const getConnection = async () => {
-    if(!pool) {
-        pool = mysql.createPool({
-            connectionLimit: 10,
-            host: MYSQL_HOST,
-            user: MYSQL_USER,
-            password: MYSQL_PASSWORD,
-            database: MYSQL_DATABASE,
-            timezone: 'Z',
-        });
+const getConnectionDB = async () => {
+    try {
+        if (!pool) {
+            pool = mysql.createPool({
+                connectionLimit: 10,
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASSWORD,
+                database: MYSQL_DATABASE,
+                timezone: 'Z',
+            });
+        }
+
+        return await pool.getConnection();
+    } catch (err) {
+        console.error(err);
+        throw new Error('Error al conectar con MySQL');
     }
-    
-    return await pool.getConnection();
 };
 
-module.exports = {
-    getConnection,
-};
+module.exports = getConnectionDB;
